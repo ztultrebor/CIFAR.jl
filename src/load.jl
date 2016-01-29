3fc6278bbeeb244292214a20e8086f02f13bbd62
Eric W. Tramel <eric.tramel@gmail.com>
2016-01-29 12:06:31 +0100

    Allow loading of all batches

--- src/load.jl
+++ src/load.jl
@@ -58,68 +58,80 @@ function toLuminance(dataset)
   return lum
 end
 
-function traindata(batch_number::Int; normalize_images=false, grey=false)
-  # Assert to the number of batches
-  @assert minimum(batch_number) >= 1 && maximum(batch_number) <= 5
-
-  # Set File Name
-  DataDir = joinpath(dirname(@__FILE__),"..","data","bin")
-  BatchFile = @sprintf "data_batch_%d.bin" batch_number
-  BatchFile = joinpath(DataDir,BatchFile)
-  LabelNameFile = joinpath(DataDir,"batches.meta.txt")
-  
-  # Fixed dataset values
+function traindata(;batch_number=-1, normalize_images=false, grey=false)
   Pixels = 1024
-  Features = 3*Pixels
-  Samples = 10000
   Labels = 10
 
-  # Initialize Dataset Structures
-  Dataset = Array(UInt8,Features,Samples)
-  Labels = Array(UInt8,Samples,1)
-  LabelNames = Array(AbstractString,10,1)
+  if batch_number > 0
+    # Assert to the number of batches  
+    @assert minimum(batch_number) >= 1 && maximum(batch_number) <= 5
 
-  # Open Batch File
-  h_BatchFile = open(BatchFile)
+    # Set File Name
+    DataDir = joinpath(dirname(@__FILE__),"..","data","bin")
+    BatchFile = @sprintf "data_batch_%d.bin" batch_number
+    BatchFile = joinpath(DataDir,BatchFile)
+    LabelNameFile = joinpath(DataDir,"batches.meta.txt")
+    
+    # Fixed dataset values
+    Features = 3*Pixels
+    Samples = 10000    
 
-  # Loop Over Samples
-  r = Array(UInt8,Pixels,1)
-  g = Array(UInt8,Pixels,1)
-  b = Array(UInt8,Pixels,1)
-  for sampleIdx = 1:Samples
-    # Read Label Byte
-    Labels[sampleIdx] = read(h_BatchFile,UInt8)+1
-    # Read Color Byte Arrays
-    read!(h_BatchFile,r)
-    read!(h_BatchFile,g)
-    read!(h_BatchFile,b)
+    # Initialize Dataset Structures
+    Dataset = Array(Float64,Features,Samples)
+    Labels = Array(UInt8,Samples,1)
+    LabelNames = Array(AbstractString,10,1)
 
-    Dataset[1:Pixels,sampleIdx] = r
-    Dataset[Pixels+1:2*Pixels,sampleIdx] = g
-    Dataset[2*Pixels+1:end,sampleIdx] = b
+    # Open Batch File
+    h_BatchFile = open(BatchFile)
+
+    # Loop Over Samples
+    r = Array(UInt8,Pixels,1)
+    g = Array(UInt8,Pixels,1)
+    b = Array(UInt8,Pixels,1)
+    for sampleIdx = 1:Samples
+      # Read Label Byte
+      Labels[sampleIdx] = read(h_BatchFile,UInt8)+1
+      # Read Color Byte Arrays
+      read!(h_BatchFile,r)
+      read!(h_BatchFile,g)
+      read!(h_BatchFile,b)
+
+      Dataset[1:Pixels,sampleIdx] = convert(Array{Float64,2},r)./256
+      Dataset[Pixels+1:2*Pixels,sampleIdx] = convert(Array{Float64,2},g)./256
+      Dataset[2*Pixels+1:end,sampleIdx] = convert(Array{Float64,2},b)./256
+    end
+
+    # Close File
+    close(h_BatchFile)
+
+    # Get Label Names
+    LabelNames = labelnames()
+
+    # Conversion to Int type
+    # Dataset = convert(Array{Int},Dataset)
+    Labels = convert(Array{Int},Labels)
+
+    # Check for grey
+    if grey
+      Dataset = toLuminance(Dataset)
+    end
+
+    # Apply Normalization
+    if normalize_images
+      Dataset = normalize(Dataset)
+    end
+  else
+    Features = grey ? Pixels : 3*Pixels
+    Dataset = Array(Float64,Features,0)
+    Labels  = []
+    for i= 1:5
+      D, L, LabelNames = traindata(batch_number=i,normalize_images=normalize_images,grey=grey)
+      Dataset = hcat(Dataset,D)
+      Labels  = [Labels;L]
+    end
   end
 
-  # Close File
-  close(h_BatchFile)
-
-  # Get Label Names
-  LabelNames = labelnames()
-
-  # Conversion to Int type
-  Dataset = convert(Array{Int},Dataset)
-  Labels = convert(Array{Int},Labels)
-
-  # Check for grey
-  if grey
-    Dataset = toLuminance(Dataset)
-  end
-
-  # Apply Normalization
-  if normalize_images
-    Dataset = normalize(Dataset)
-  end
-
-  return Dataset, Labels, LabelNames
+  return Dataset,Labels,LabelNames
 end
 
 
